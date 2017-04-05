47f470f47c4e285376e440856a09abfb2558824a
Julien Daigle <julzmeister@gmail.com>
2017-04-05 14:26:24 -0400

    Changed Add NPC implementation -changed it from using Ruby code to only SQL statements -also deleted all the old stuff about uploading websites etc

--- app.rb
+++ app.rb
@@ -97,122 +97,23 @@ post '/login' do
     end
 end
 
-post '/uploadUsers' do
-  halt(401, 'Not Authorized') unless session[:role] == 'DM'
-
-  begin
-    File.open(params['file_source'][:filename], 'wb') do |f|
-      f.write(params['file_source'][:tempfile].read)
-    end
-
-    file_name = params['file_source'][:filename].to_s
-
-    original = File.open(file_name, 'r') { |file| file.readlines }
-    blankless = original.reject{ |line| line.match(/^$/) }
-
-    File.open(file_name, 'w') do |file|
-      blankless.each { |line| file.puts line }
-    end
-
-    userList = CSV.read(file_name)
-    userList.each do |x|
-      password = x[1].to_s
-      DungDrags.create(:username => x[0], :password => password, :role => x[2])
-    end
-    File.delete(params['file_source'][:filename].to_s)
-  rescue
-    puts 'Error: Invalid file upload'
-  end
-
-  redirect '/dm'
-end
-
-#Used for extracting zip files
-def extract_zip(file, destination)
-  FileUtils.mkdir_p(destination)
-
-  Zip::File.open(file) do |zip_file|
-    zip_file.each { |f|
-      f_path=File.join(destination, f.name)
-      FileUtils.mkdir_p(File.dirname(f_path))
-      zip_file.extract(f, f_path) unless File.exist?(f_path)
-    }
-  end
-end
-
-post '/uploadWebsites' do
-  halt(401, 'Not Authorized') unless session[:role] == 'DM'
-  begin
-    File.open(params['file_source'][:filename], 'wb') do |f|
-      f.write(params['file_source'][:tempfile].read)
-    end
-    file_name = params['file_source'][:filename].to_s
-    file_name = URI.encode(file_name)
-    destination = './public/files/'
-    extract_zip(file_name, destination)
-    File.delete(file_name)
-    redirect '/displayWebsites'
-
-  rescue Exception => error
-    puts error.message
-    puts error.backtrace.inspect
-    redirect '/dm'
-  end
-
-end
-
-post '/exportCSV' do
-  halt(401, 'Not Authorized') unless session[:role] == 'DM'
-  begin
-    @votingData = DungDrags.all #Stores all data into variable
-    @csv = CSV.generate do |csv| #Generates CSV format
-      @votingData.each {|x|
-        csv << ["#{x.username.to_s}","#{x.first_Pick.to_s}","#{x.second_Pick.to_s}","#{x.third_Pick.to_s}"]
-      }
-    end
-    @dataArray = CSV.parse(@csv) #Convert CSV to an array
-
-    File.open('VotingData.csv','wb'){ |x| #Export CSV using the above array
-      x << @dataArray.map(&:to_csv).join
-    }
-
-    send_file('VotingData.csv', :filename => 'VotingData.csv', :type => 'application/csv')
-    File.delete('VotingData.csv')
-
-    puts '****** CSV file was successfully exported ******'
-  rescue
-    puts '****** CSV was unsuccessful in export ******'
-
-  end
-
-  redirect '/dm'
-end
-
-
-get '/user/:username' do
-  begin
-    @user = DungDrags.get(params[:username])
-    halt(401, 'Invalid User Access!') unless (session[:name].casecmp(@user.username) == 0)
-
-    #You use save to persist changes made to a loaded resource and
-    # you use update when you want to immediately persist changes
-    # without changing resource's state to 'dirty'.
-    @user.save
-    erb :querypage
-  rescue
-    puts '****** TEST FLAG!! ******'
-    @user = DungDrags.get(params[:username])
-    erb :home
-  end
-
-
-end
 
 post '/addNPC' do
   halt(401, 'Not Authorized') unless session[:role] == 'DM'
   begin
-    NPC_stats.create(:Race => params[:Race], :Alignment => params[:Alignment], :Type => params[:Type], :Charisma => params[:Charisma], :Wisdom => params[:Wisdom], :Intelligence => params[:Intelligence], :Constitution => params[:Constitution], :Dexterity => params[:Dexterity], :Strength => params[:Strength])
-    NPC.create(:name => params[:Name], :in_Campaign => false)
+    db = SQLite3::Database.new('development.db')
+    db.type_translation = true
+    # get highest npc id and increment by 1 to get new ID
+    newID = db.get_first_value('select max(npc_id) from npcs')
+    newID += 1
+    # add into npcs table
+    db.execute('insert into npcs values(?,?,?)', [newID, params[:Name], 'f'])
+    # add into npc_stats table
+    db.execute('insert into npc_stats values(?,?,?,?,?,?,?,?,?,?)',
+               [newID, params[:Race], params[:Alignment], params[:Type],
+                params[:Charisma], params[:Wisdom], params[:Intelligence],
+                params[:Constitution], params[:Dexterity], params[:Strength]])
+    db.close
   end
   redirect '/dm'
 end
